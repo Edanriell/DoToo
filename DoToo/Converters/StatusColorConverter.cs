33ca9657ab88e0003677c5a7639917f3bc50de28
Edanriell <Edanriell@gmail.com>
2025-06-28 14:12:09 +0300

    feature(value-converter): implemented

---
+++ DoToo/Converters/StatusColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace DoToo.Converters;
+
+public class StatusColorConverter : IValueConverter
+{
+    public object Convert(
+        object value, Type targetType, object
+            parameter, CultureInfo culture)
+    {
+        return (Color)Application.Current.Resources[
+            (bool)value ? "CompletedColor" : "ActiveColor"];
+    }
+
+    public object ConvertBack(
+        object value, Type targetType,
+        object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
