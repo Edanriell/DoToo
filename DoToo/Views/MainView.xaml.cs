48f20e4ee62dcbad99cb4d66c2fa6f111e77c432
Edanriell <Edanriell@gmail.com>
2025-06-28 12:08:30 +0300

    feature(main-view): created

---
+++ DoToo/Views/MainView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using DoToo.ViewModels;
+
+namespace DoToo.Views;
+
+public partial class MainView : ContentPage
+{
+    public MainView(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        viewModel.Navigation = Navigation;
+        BindingContext = viewModel;
+    }
+}
