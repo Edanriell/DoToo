b3ad1a9d25b7ce906b8d39a2b58e139060a5ba6e
Edanriell <Edanriell@gmail.com>
2025-06-28 11:54:02 +0300

    feature(todoitem-view-model): created

---
+++ DoToo/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DoToo.Models;
+
+namespace DoToo.ViewModels;
+
+public partial class TodoItemViewModel : ViewModel
+{
+    [ObservableProperty] private TodoItem item;
+
+    public TodoItemViewModel(TodoItem item) { Item = item; }
+
+    public string StatusText => Item.Completed ? "Reactivate" : "Completed";
+
+    public event EventHandler ItemStatusChanged;
+}
