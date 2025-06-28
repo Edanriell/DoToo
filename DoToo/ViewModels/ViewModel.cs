b918d4fb69d1dcb7974fb3ce601aae9d77193b40
Edanriell <Edanriell@gmail.com>
2025-06-28 11:45:23 +0300

    feature(view-model): created

---
+++ DoToo/ViewModels/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DoToo.ViewModels;
+
+// public abstract class ViewModel : INotifyPropertyChanged
+// {
+//     public event PropertyChangedEventHandler PropertyChanged;
+//
+//     public void RaisePropertyChanged(params string[] propertyNames)
+//     {
+//         foreach (var propertyName in propertyNames)
+//         {
+//             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//         }
+//     }
+//
+//     public INavigation Navigation { get; set; }
+// }
+
+[ObservableObject]
+public abstract partial class ViewModel
+{
+    public INavigation Navigation { get; set; }
+}
